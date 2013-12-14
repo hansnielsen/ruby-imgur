f9c045ab7d6fe20ecdf410efa39c13967c2f0150
Hans Nielsen <hans@stackallocated.com>
2013-12-13 23:50:23 -0800

    Be more careful about what settings are accepted

--- lib/imgur/requests/update_account_settings.rb
+++ lib/imgur/requests/update_account_settings.rb
@@ -1,6 +1,10 @@
 class Imgur::Client
   class Real
     def update_account_settings(params={})
+      params = params.dup.select do |k,v|
+        [:bio, :public_images, :messaging_enabled,
+         :album_privacy, :accepted_gallery_terms].include? k
+      end
       path = "/account/me/settings"
       request(
         :method => :post,
